09eef29b8b589d2b115607abd8c008ba456376fa
OceanTour <51856544+Stardream-Noel@users.noreply.github.com>
2022-12-23 22:34:53 +0800

    ...

--- app/SoulCrystal.Benchmark/Program.cs
+++ app/SoulCrystal.Benchmark/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace SoulCrystal.Benchmark
+﻿namespace SoulCrystal.Benchmark;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
