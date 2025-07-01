802190e1e23402d5d7cdf8f4bc47c1aafda1de76
Miling <fne1013638645@gmail.com>
2025-07-01 23:12:32 +0800

    net6.0 -> net9.0

---
+++ app/SoulCrystal.Webstie.New/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SoulCrystal.Webstie.New.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
